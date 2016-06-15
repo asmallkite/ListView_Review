5e4782c53caa79fdf64d95502f802872bf39c4e8
Li Zheng <lixiaozheng.good@163.com>
2016-06-15 20:25:52 +0800

    add OnclickItem

--- app/src/main/java/com/example/a10648/recover/MainActivity.java
+++ app/src/main/java/com/example/a10648/recover/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.a10648.recover;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -19,6 +23,13 @@ public class MainActivity extends ActionBarActivity {
         SizeAdapter sizeAdapter = new SizeAdapter(this, R.layout.size_item, sizeArrayList);
         ListView listView = (ListView)findViewById(R.id.list_view);
         listView.setAdapter(sizeAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Size big_small =sizeArrayList.get(position);
+                Toast.makeText(MainActivity.this, big_small.getName(), Toast.LENGTH_SHORT).show();
+            }
+        });
 
     }
     private void initData(){
