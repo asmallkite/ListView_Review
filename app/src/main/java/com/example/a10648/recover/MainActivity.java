c9bf90ab57431a25fd8f2ca8ee756bffbb0247c1
Li Zheng <lixiaozheng.good@163.com>
2016-06-15 20:03:45 +0800

    修复一些细节问题

--- app/src/main/java/com/example/a10648/recover/MainActivity.java
+++ app/src/main/java/com/example/a10648/recover/MainActivity.java
@@ -1,21 +1,10 @@
 package com.example.a10648.recover;
 
-import android.content.Context;
-import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.ListView;
-import android.widget.Toast;
 
 import java.util.ArrayList;
-import java.util.logging.Logger;
 
 public class MainActivity extends ActionBarActivity {
     public static final String TAG = MainActivity.class.getSimpleName();
@@ -26,14 +15,15 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initData();
         SizeAdapter sizeAdapter = new SizeAdapter(this, R.layout.size_item, sizeArrayList);
         ListView listView = (ListView)findViewById(R.id.list_view);
         listView.setAdapter(sizeAdapter);
 
     }
     private void initData(){
-        Size big = new Size(R.drawable.add, "Big");
-        Size small = new Size(R.drawable.remove, "small");
+        Size big = new Size(R.drawable.big, "Big");
+        Size small = new Size(R.drawable.small, "small");
         sizeArrayList.add(big);
         sizeArrayList.add(small);
     }
